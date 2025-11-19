8700fa332c416ff42b9a8a0ab37acaca70213567
Paul <paul.quidu@mail.mcgill.ca>
2025-11-18 23:33:21 -0500

    Include Buzzfeed as a left skew  newspaper

--- src/newspaper_distribution.py
+++ src/newspaper_distribution.py
@@ -30,14 +30,14 @@ left_sources = {
     "Washington Post",
     "HuffPost", "Raw Story",
     "TheWrap",
+    "Buzzfeed"
 }
 
 centrist_sources = {
     "CBS News", "CNBC",
     "ABC News",
     "Reuters",
-    "USA Today",
-    "Buzzfeed"
+    "USA Today"
 }
 
 right_sources = {
